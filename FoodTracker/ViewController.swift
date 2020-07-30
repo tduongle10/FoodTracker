eec67d4922241dce41345c580c75da16e74a9d75
Duong Le <tduongle10@gmail.com>
2020-07-30 21:59:35 +0700

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Duong Le on 7/30/20.
+//  Copyright © 2020 Duong Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
